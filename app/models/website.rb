92b4c8b19850e70d104c06c1d0a6e436393806e1
plusjade <plusjade@gmail.com>
2011-03-24 00:09:17 -0700

    add url normalizer

--- app/models/website.rb
+++ app/models/website.rb
@@ -11,4 +11,28 @@ class Website
   has n, :users, self, :through => :users_links, :via => :user
   has n, :usees, self, :through => :usees_links, :via => :website
 
+  before :save do
+    self.url = normalize_url(self.url.blank? ? self.domain : self.url)
+  end
+
+
+  def valid_url
+    if normalize_url(self.website).nil?
+      [ false, "Please provide a valid website url." ]
+    else
+      return true
+    end
+  end
+  
+  def normalize_url(url)
+    # url contains http:// or https://
+    unless url =~ /(https|http)+(:\/\/)/i 
+      url = 'http://' + url
+    end  
+    url = URI::parse(url).normalize.to_s
+    return url
+  rescue URI::InvalidURIError => e
+    return nil
+  end
+    
 end
