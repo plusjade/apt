dfb5330206c4b65d7e40cdc57fb8115609e83b0c
plusjade <plusjade@gmail.com>
2011-03-25 03:31:16 -0700

    changed some names and add comments to the assocations

--- app/models/website.rb
+++ app/models/website.rb
@@ -5,36 +5,71 @@ class Website
   property :domain, String
   property :url, String
   
-  has n, :users_links, WebsitesUser, :child_key => [:website_id]
-  has n, :usees_links, WebsitesUser, :child_key => [:user_id]
+  has n, :user_associations, WebsitesUser, :child_key => [:website_id]
+  has n, :usee_associations, WebsitesUser, :child_key => [:user_id]
   
-  has n, :users, self, :through => :users_links, :via => :user
-  has n, :usees, self, :through => :usees_links, :via => :website
+  has n, :users, self, :through => :user_associations, :via => :user
+  has n, :usees, self, :through => :usee_associations, :via => :website
 
   before :save do
     self.url = normalize_url(self.url.blank? ? self.domain : self.url)
   end
-
+  
   def usedby_data
     {
       :meta => {
-        :type => :usedby,
-        :total => self.users.count
+        :type   => :usedby,
+        :total  => self.user_associations.count
       },
-      :websites => ActiveSupport::JSON.decode(self.users.to_json)
+      :websites => self.users_with_comments
     }
   end
-
+  
   def uses_data
     {
       :meta => {
         :type => :uses,
-        :total => self.usees.count
+        :total => self.usee_associations.count
       },
-      :websites => ActiveSupport::JSON.decode(self.usees.to_json)
+      :websites => self.usees_with_comments 
     }
   end
   
+
+  def users_with_comments
+    ids = []
+    comments = {}
+    user_associations.map do |w|
+      ids.push(w.user_id)
+      comments[w.user_id] = w.comment
+    end
+    
+    websites = []
+    Website.all(:id => ids).map do |w| 
+      websites.push(w.attributes.merge(:comment_by => comments[w.id]))
+    end
+
+    websites
+  end
+  
+  
+  def usees_with_comments
+    ids = []
+    comments = {}
+    usee_associations.map do |w|
+      ids.push(w.website_id)
+      comments[w.website_id] = w.comment
+    end
+
+    websites = []
+    Website.all(:id => ids).map do |w| 
+      websites.push(w.attributes.merge(:comment_on => comments[w.id]))
+    end
+
+    websites
+  end
+  
+    
   def valid_url
     if normalize_url(self.website).nil?
       [ false, "Please provide a valid website url." ]
--- app/models/websites_user.rb
+++ app/models/websites_user.rb
@@ -3,7 +3,9 @@ class WebsitesUser
 
   property :website_id, Integer, :key => true, :min => 1
   property :user_id, Integer, :key => true, :min => 1
-
+  property :comment, Text, :lazy => false
+  
+  
   belongs_to :website, :key => true
   belongs_to :user, Website, :key => true
 end
