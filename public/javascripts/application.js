22296fb8b797a22ddf692fa075742ff27c8c1bc4
plusjade <plusjade@gmail.com>
2011-03-29 08:04:35 -0700

    basic styling, add jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,33 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var simpleTabs = {
+  $list : null,
+  $wrapper : null,
+  init : function(list, wrapper){
+    simpleTabs.$list = list;
+    simpleTabs.$wrapper = wrapper;
+    
+    simpleTabs.$list.find("a").click(function(){
+      simpleTabs.showTab($(this));
+      return false;
+    });
+    
+    simpleTabs.showFirstTab();
+  },
+  
+  showFirstTab : function(){
+    var $first = simpleTabs.$list.find("a:first");
+    simpleTabs.showTab($first);
+  },
+  
+  showTab : function(node){
+    simpleTabs.clear();
+    
+    var tabIndex = node.parent().index();
+    simpleTabs.$wrapper.find("div.tabs").eq(tabIndex).show();
+    node.addClass("active");
+  },
+  
+  clear : function(){
+    simpleTabs.$list.find("a").removeClass("active");
+    simpleTabs.$wrapper.find("div.tabs").hide();
+  }
+}
