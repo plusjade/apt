ac8b96f1e433decec668e5d10c753c6be7e3baf1
plusjade <plusjade@gmail.com>
2011-04-04 23:48:12 -0700

    update user association form to work with current_account

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -3,7 +3,10 @@ class GraphController < ApplicationController
   before_filter :load_website
   
   def index
-    
+    if current_account
+      @user_website = current_account.website
+      @user_association = @website.user_associations.first(:user_id => @user_website.id)
+    end
   end
   
   def profile
